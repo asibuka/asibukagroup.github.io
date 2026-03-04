78659aad9b93b596bed4e1ee1d1294a5fe30a281
ASIBUKA Group <me@asibuka.com>
2026-03-05 04:24:24 +0700

    final 2

--- _includes/ads/adsterra-popunder.js
+++ _includes/ads/adsterra-popunder.js
@@ -1,4 +1,4 @@
 {% unless page.is_amp %}
 <div itemscope itemtype='https://schema.org/WPAdblock' class='adsterra-popunder-ad'></div>
-<script>Defer.dom(".adsterra-popunder-ad",1e3,"loaded",function(){var e=document.createElement("script");e.src="https://pl28770414.effectivegatecpm.com/0b/cc/21/0bcc210b115c30cf38b869cd766dd43f.js",document.body.appendChild(e),console.info("Adsterra Popunder ad is loaded.")},{rootMargin:"150%"})</script>
+<script>Defer.dom(".adsterra-popunder-ad",1000,"loaded",function(){var a=document.createElement("script");a.src="https://pl28770414.effectivegatecpm.com/0b/cc/21/0bcc210b115c30cf38b869cd766dd43f.js",document.body.appendChild(a),console.info("Adsterra Popunder ad is loaded.")},{rootMargin:"150%"});</script>
 {% endunless %}
