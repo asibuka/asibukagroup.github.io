34f1cad6604570e6d4fdb6ef6118d6cecde136d1
ASIBUKA Group <me@asibuka.com>
2026-03-05 03:46:15 +0700

    part 3

--- _includes/ads/adsterra-banner.js
+++ _includes/ads/adsterra-banner.js
@@ -1,5 +1,39 @@
 {% unless page.is_amp %}
 <div itemscope itemtype='https://schema.org/WPAdblock' class='adsterra-300x250-banner-ad ads text-center'>
 </div>
-<script>Defer.dom(".adsterra-300-x250-banner-ad",1e3,"loaded",function(){var e,a=document.querySelector(".adsterra-300-x250-banner-ad");a?((e=document.createElement("script")).textContent=`,atOptions={key:"6a0bf86bb4a27e1fdf72b88f3b82befc",format:"iframe",height:250,width:300,params:{}},`,a.appendChild(e),(e=document.createElement("script")).src="https://www.highperformanceformat.com/6a0bf86bb4a27e1fdf72b88f3b82befc/invoke.js",e.async=!0,a.appendChild(e),console.info("Adsterra 300x250 Loaded.")):console.warn("Container element not found.")},{rootMargin:"150%"});</script>
+<script>
+    Defer.dom(
+  ".adsterra-300-x250-banner-ad",
+  1e3,
+  "loaded",
+  function () {
+    var container = document.querySelector(".adsterra-300-x250-banner-ad");
+
+    if (!container) {
+      console.warn("Container element not found.");
+      return;
+    }
+    var optionsScript = document.createElement("script");
+    optionsScript.textContent = `
+      atOptions = {
+        key: "6a0bf86bb4a27e1fdf72b88f3b82befc",
+        format: "iframe",
+        height: 250,
+        width: 300,
+        params: {}
+      };
+    `;
+    container.appendChild(optionsScript);
+    var invokeScript = document.createElement("script");
+    invokeScript.src =
+      "https://www.highperformanceformat.com/6a0bf86bb4a27e1fdf72b88f3b82befc/invoke.js";
+    invokeScript.async = true;
+
+    container.appendChild(invokeScript);
+
+    console.info("Adsterra 300x250 Loaded.");
+  },
+  { rootMargin: "150%" }
+);
+</script>
 {% endunless %}
