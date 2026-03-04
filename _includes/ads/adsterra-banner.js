4414f80691c4d2eb42f8a550a15cf41cbc9eaa27
ASIBUKA Group <me@asibuka.com>
2026-03-05 04:08:15 +0700

    part 3

--- _includes/ads/adsterra-banner.js
+++ _includes/ads/adsterra-banner.js
@@ -1,5 +1,14 @@
 {% unless page.is_amp %}
 <div itemscope itemtype='https://schema.org/WPAdblock' class='adsterra-300x250-banner-ad ads text-center'>
+<script>
+  atOptions = {
+    'key' : '6a0bf86bb4a27e1fdf72b88f3b82befc',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+  };
+</script>
 </div>
 <script>
     Defer.dom(
@@ -13,17 +22,6 @@
       console.warn("Container element not found.");
       return;
     }
-    var optionsScript = document.createElement("script");
-    optionsScript.textContent = `
-      atOptions = {
-        key: "6a0bf86bb4a27e1fdf72b88f3b82befc",
-        format: "iframe",
-        height: 250,
-        width: 300,
-        params: {}
-      };
-    `;
-    container.appendChild(optionsScript);
     var invokeScript = document.createElement("script");
     invokeScript.src =
       "https://www.highperformanceformat.com/6a0bf86bb4a27e1fdf72b88f3b82befc/invoke.js";
--- _includes/ads/mondiad-banner.js
+++ _includes/ads/mondiad-banner.js
@@ -1,6 +1,27 @@
 {% unless page.is_amp %}
 <div class='ads text-center'>
 <div class='mondiad-300x250-banner-ad' data-mndbanid="ecf71b32-c495-4156-ba3f-a73888b5af25"></div>
-<script>Defer.dom(".mondiad-300x250-banner-ad",1000,"loaded",function(){var a=document.createElement("script");a.src="https://ss.mrmnd.com/banner.js",document.body.appendChild(a),console.info("Mondiad 300x250 Banner ad is loaded.")},{rootMargin:"150%"});</script>
+<script>Defer.dom(
+  ".mondiad-300x250-banner-ad",
+  1000,
+  "loaded",
+  function () {
+    var container = document.querySelector(".mondiad-300x250-banner-ad");
+
+    if (!container) {
+      console.warn("Mondiad container not found.");
+      return;
+    }
+
+    var a = document.createElement("script");
+    a.src = "https://ss.mrmnd.com/banner.js";
+    a.async = true;
+
+    container.appendChild(a);
+
+    console.info("Mondiad 300x250 Banner ad is loaded.");
+  },
+  { rootMargin: "150%" }
+);</script>
 </div>
 {% endunless %}
