2d0eeda2cdc45389bf511c61cead62f1ecd0e53a
ASIBUKA Group <me@asibuka.com>
2026-03-04 20:28:49 +0700

    fix some ads

--- _includes/ads/adsterra-banner.js
+++ _includes/ads/adsterra-banner.js
@@ -2,5 +2,5 @@
 <div itemscope itemtype='https://schema.org/WPAdblock' class='adsterra-300x250-banner-ad ads text-center'>
 <script>atOptions={key:"6a0bf86bb4a27e1fdf72b88f3b82befc",format:"iframe",height:250,width:300,params:{}};</script>
 </div>
-<script>Defer.dom(".adsterra-300x250-banner-ad",1000,"loaded",function(){var a=document.createElement("script");a.src="https://www.highperformanceformat.com/6a0bf86bb4a27e1fdf72b88f3b82befc/invoke.js",document.body.appendChild(a),console.info("Adsterra 300x250 Banner ad is loaded.")},{rootMargin:"150%"});</script>
+<script>Defer.dom(".adsterra-300-x250-banner-ad",1e3,"loaded",function(){var e,n=document.querySelector(".adsterra-300-x250-banner-ad");n?((e=document.createElement("script")).src="https://www.highperformanceformat.com/6a0bf86bb4a27e1fdf72b88f3b82befc/invoke.js",n.appendChild(e),console.info("Loaded.")):console.warn("Container element not found.")},{rootMargin:"150%"});</script>
 {% endunless %}
