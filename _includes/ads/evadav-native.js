9fbf6b2241dcccfcd7ae1fd367481d86e2f68435
ASIBUKA Group <me@asibuka.com>
2026-03-04 19:35:21 +0700

    edit evadav ads

--- _includes/ads/evadav-native.js
+++ _includes/ads/evadav-native.js
@@ -1,7 +1,23 @@
 {% unless page.is_amp %}
+<<<<<<< HEAD
+<div class='evadav-native-ad'></div>
+<script>Defer.dom(".evadav-native-ad", 1000, "loaded", function () {
+    var container = document.querySelector(".evadav-popup-push-ad");
+    
+    if (container) {
+        var a = document.createElement("script");
+        a.src = "https://curoax.com/na/waWQiOjExOTc4NzksInNpZCI6MTQ2MDQwOSwid2lkIjo3MTA5NzgsInNyYyI6Mn0=eyJ.js";
+        container.appendChild(a);
+        console.info("Loaded.");
+    } else {
+        console.warn("Container element not found.");
+    }
+}, { rootMargin: "150%" });</script>
+=======
 <noscript class='noscript'>
 <div itemscope itemtype='https://schema.org/WPAdblock' class='ads text-center'>
 <script async src="https://curoax.com/na/waWQiOjExOTc4NzksInNpZCI6MTQ2MDQwOSwid2lkIjo3MTA5NzgsInNyYyI6Mn0=eyJ.js"></script>
 </div>
 </noscript>
+>>>>>>> e0de9d44ae9ef9a9dd989d20cf5f2b0d24c65898
 {% endunless %}
