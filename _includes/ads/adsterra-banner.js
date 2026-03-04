cd24f78a629e3510817d23e3f219383ea7f102de
ASIBUKA Group <me@asibuka.com>
2026-03-05 04:11:20 +0700

    part 4

--- _includes/ads/adsterra-banner.js
+++ _includes/ads/adsterra-banner.js
@@ -1,18 +1,9 @@
 {% unless page.is_amp %}
 <div itemscope itemtype='https://schema.org/WPAdblock' class='adsterra-300x250-banner-ad ads text-center'>
-<script>
-  atOptions = {
-    'key' : '6a0bf86bb4a27e1fdf72b88f3b82befc',
-    'format' : 'iframe',
-    'height' : 250,
-    'width' : 300,
-    'params' : {}
-  };
-</script>
 </div>
 <script>
     Defer.dom(
-  ".adsterra-300-x250-banner-ad",
+  ".adsterra-300x250-banner-ad",
   1e3,
   "loaded",
   function () {
@@ -22,6 +13,17 @@
       console.warn("Container element not found.");
       return;
     }
+    var optionsScript = document.createElement("script");
+    optionsScript.textContent = `
+      atOptions = {
+        key: "6a0bf86bb4a27e1fdf72b88f3b82befc",
+        format: "iframe",
+        height: 250,
+        width: 300,
+        params: {}
+      };
+    `;
+    container.appendChild(optionsScript);
     var invokeScript = document.createElement("script");
     invokeScript.src =
       "https://www.highperformanceformat.com/6a0bf86bb4a27e1fdf72b88f3b82befc/invoke.js";
