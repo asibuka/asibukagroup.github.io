3a56f837228aa6adffa7b79a1833e583c349ec7b
ASIBUKA Group <me@asibuka.com>
2026-03-05 08:44:05 +0700

    fix evadav native push

--- _includes/ads/evadav-native-push.js
+++ _includes/ads/evadav-native-push.js
@@ -1,4 +1,4 @@
 {% unless page.is_amp %}
 <div itemscope itemtype='https://schema.org/WPAdblock' class='evadav-native-push-ad ads text-center'></div>
-<script>Defer.dom(".evadav-native-push-ad",1000,"loaded",function(){var a=document.createElement("script");a.src="https://curoax.com/na/waWQiOjExOTc4NzksInNpZCI6MTQ2MDQwOSwid2lkIjo3MTA5NzgsInNyYyI6Mn0=eyJ.js",document.body.appendChild(a),console.info("Evadav Native Push ad is loaded.")},{rootMargin:"150%"});</script>
+<script>Defer.dom(".evadav-native-push-ad",1000,"loaded",function(){var a=document.createElement("script");a.src="https://puabvo.com/code/silent.js?h=waWQiOjExOTc4NzksInNpZCI6MTQ2MDQwOSwid2lkIjo3MTA5NzksInNyYyI6Mn0=eyJ",document.body.appendChild(a),console.info("Evadav Native Push ad is loaded.")},{rootMargin:"150%"});</script>
 {% endunless %}
