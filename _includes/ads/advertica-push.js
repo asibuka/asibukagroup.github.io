06ada9c0ba43adf29fb4f1a1086908b0612193cd
ASIBUKA Group <me@asibuka.com>
2026-03-06 04:49:51 +0700

    part 3

---
+++ _includes/ads/advertica-push.js
@@ -0,0 +1,4 @@
+{% unless page.is_amp %}
+<div itemscope itemtype='https://schema.org/WPAdblock' class='advertica-push-ad'></div>
+<script>Defer.dom(".advertica-push-ad",1000,"loaded",function(){var a=document.createElement("script");a.src="//cdn-server.live/b8e2bb065f6d1a5f614e/f6bc2019e9/?placementName=default",document.body.appendChild(a),console.info("Advertica Push ad is loaded.")},{rootMargin:"150%"});</script>
+{% endunless %}
---
+++ _includes/ads/advertica-tabunder.js
@@ -0,0 +1,4 @@
+{% unless page.is_amp %}
+<div itemscope itemtype='https://schema.org/WPAdblock' class='advertica-tabunder-ad'></div>
+<script>Defer.dom(".advertica-tabunder-ad",1000,"loaded",function(){var a=document.createElement("script");a.src="//data527.click/ab8ac3b77ff2b007ce94/5cb4ba5b33/?placementName=tabunder",document.body.appendChild(a),console.info("Advertica Tabunder ad is loaded.")},{rootMargin:"150%"});</script>
+{% endunless %}
