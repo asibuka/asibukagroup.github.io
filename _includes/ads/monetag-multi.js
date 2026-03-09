fc8b5d40b503a2b075456abe85d89ed2cc948f4d
ASIBUKA Group <me@asibuka.com>
2026-03-10 04:44:46 +0700

    add monetag multi ads

---
+++ _includes/ads/monetag-multi.js
@@ -0,0 +1,4 @@
+{% unless page.is_amp %}
+<div itemscope itemtype='https://schema.org/WPAdblock' class='monetag-multi-ad ads text-center'></div>
+<script>Defer.dom(".monetag-multi-ad",1000,"loaded",function(){var a=document.createElement("script");a.src="https://quge5.com/88/tag.min.js";a.dataset.zone='146175',document.body.appendChild(a),console.info("Monetag Multi ad is loaded.")},{rootMargin:"150%"});</script>
+{% endunless %}
