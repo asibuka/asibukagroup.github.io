5d09ec80752ac955e99e14c73da3206ace341be7
ASIBUKA Group <me@asibuka.com>
2026-03-10 11:09:52 +0700

    update monetag ads

---
+++ _includes/ads/monetag-inpage-push.js
@@ -0,0 +1,4 @@
+{% unless page.is_amp %}
+<div itemscope itemtype='https://schema.org/WPAdblock' class='monetag-inpage-push-ad ads text-center'></div>
+<script>Defer.dom(".monetag-inpage-push-ad",1000,"loaded",function(){var a=document.createElement("script");a.src="https://nap5k.com/tag.min.js";a.dataset.zone='9302599',document.body.appendChild(a),console.info("Monetag In Page Push ad is loaded.")},{rootMargin:"150%"});</script>
+{% endunless %}
