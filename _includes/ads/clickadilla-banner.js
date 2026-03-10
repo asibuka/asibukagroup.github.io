d8720ffa8a2c8fbbb81f76a12dfa3af095ebb654
ASIBUKA Group <me@asibuka.com>
2026-03-10 11:43:15 +0700

    edit clickadilla

--- _includes/ads/clickadilla-banner.js
+++ _includes/ads/clickadilla-banner.js
@@ -2,5 +2,5 @@
 <div itemscope itemtype='https://schema.org/WPAdblock' class='ads text-center'>
 <div class='clickadilla-banner-ad' data-banner-id="1447384"></div>
 </div>
-<script>Defer.dom(".clickadilla-banner-ad",1e3,"loaded",function(){var e,n=document.querySelector(".clickadilla-banner-ad");n?((e=document.createElement("script")).src="https://js.wpadmngr.com/static/adManager.js";a.setAttribute('data-admpid','323807'),n.appendChild(e),console.info("Mondiad Banner Loaded.")):console.warn("Container element not found.")},{rootMargin:"150%"});</script>
+<script>Defer.dom(".clickadilla-banner-ad",1e3,"loaded",function(){var e,n=document.querySelector(".clickadilla-banner-ad");n?((e=document.createElement("script")).src="https://js.wpadmngr.com/static/adManager.js";e.setAttribute('data-admpid','323807'),n.appendChild(e),console.info("Mondiad Banner Loaded.")):console.warn("Container element not found.")},{rootMargin:"150%"});</script>
 {% endunless %}
