a758167d132e7f339092d56b236b17e7ff66e320
ASIBUKA Group <me@asibuka.com>
2026-03-25 06:09:35 +0700

    minor updates

--- _includes/ads/adsense-display.js
+++ _includes/ads/adsense-display.js
@@ -2,7 +2,7 @@
 {% if page.is_amp %}
 <amp-ad width="100vw" height="320" type="adsense" data-ad-client="ca-pub-4951061355072034" data-ad-slot="4462286938" data-auto-format="rspv" data-full-width=""><div overflow=""></div></amp-ad>
 {% else %}
-<ins class="adsbygoogle" style="display:block" data-full-width-responsive="true" data-ad-format="auto" data-ad-client="ca-pub-4951061355072034" data-ad-slot="4462286938"></ins>
+<ins class="adsbygoogle" data-full-width-responsive="true" data-ad-format="auto" data-ad-client="ca-pub-4951061355072034" data-ad-slot="4462286938"></ins>
 <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
 {% endif %}
 </div>
