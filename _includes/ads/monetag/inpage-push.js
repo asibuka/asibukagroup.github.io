51cde7e2fab5148393cdad6d448407eef03211f5
ASIBUKA Group <me@asibuka.com>
2026-04-21 01:58:50 +0700

    update

--- _includes/ads/monetag/inpage-push.js
+++ _includes/ads/monetag/inpage-push.js
@@ -1,4 +1,4 @@
 {% unless page.is_amp %}
 <div itemscope itemtype='https://schema.org/WPAdblock' class='monetag-inpage-push-ad ads text-center'></div>
-<script>Defer.dom(".monetag-inpage-push-ad",1e3,"loaded",function(){var c=document.querySelector(".monetag-inpage-push-ad");if(!c){console.warn("Container element not found.");return;}var s=document.createElement("script");s.src="https://nap5k.com/tag.min.js";s.async=true;s.setAttribute('data-zone','9302599');c.appendChild(s);console.info("Monetag In Page Push Loaded.")},{rootMargin:"150%"})</script>
+<script>Defer.dom(".monetag-inpage-push-ad",1e3,"loaded",function(){var c=document.querySelector(".monetag-inpage-push-ad");if(!c){console.warn("Container element not found.");return;}var s=document.createElement("script");s.src="https://nap5k.com/tag.min.js";s.async=true;s.setAttribute('data-zone','10700137');c.appendChild(s);console.info("Monetag In Page Push Loaded.")},{rootMargin:"150%"})</script>
 {% endunless %}
